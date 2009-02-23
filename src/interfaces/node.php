ebf40bec6fe66bd0e55cdd8527ceb0ebb6a7c616
Derick Rethans <github@derickrethans.nl>
2009-02-23 11:21:38 +0000

    - s/2008/2009.

--- src/interfaces/node.php
+++ src/interfaces/node.php
@@ -8,7 +8,7 @@
  *
  * @package Workflow
  * @version //autogen//
- * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
 
--- tests/execution_test.php
+++ tests/execution_test.php
@@ -3,7 +3,7 @@
  * @package Workflow
  * @subpackage Tests
  * @version //autogentag//
- * @copyright Copyright (C) 2005-2008 eZ systems as. All rights reserved.
+ * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
 
