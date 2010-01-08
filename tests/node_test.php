dbad61305b33fdc7aa190fffb43a84f8d8f72053
Derick Rethans <github@derickrethans.nl>
2010-01-08 15:27:47 +0000

    - s/2009/2010/g

--- tests/node_test.php
+++ tests/node_test.php
@@ -3,7 +3,7 @@
  * @package Workflow
  * @subpackage Tests
  * @version //autogentag//
- * @copyright Copyright (C) 2005-2009 eZ Systems AS. All rights reserved.
+ * @copyright Copyright (C) 2005-2010 eZ Systems AS. All rights reserved.
  * @license http://ez.no/licenses/new_bsd New BSD License
  */
 
